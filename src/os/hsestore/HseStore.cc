40dbc9c5b49e73d0229f9fbe28390f4a0261a5fa
Tristan Partin <tpartin@micron.com>
2021-03-15 11:26:53 -0500

    Add dout logging

--- src/os/hsestore/HseStore.cc
+++ src/os/hsestore/HseStore.cc
@@ -15,6 +15,7 @@
 #include <string_view>
 #include <hse/hse.h>
 
+#include "common/debug.h"
 #include "HseStore.h"
 
 #define dout_context cct
@@ -43,32 +44,38 @@ int HseStore::mount()
 
   rc = hse_kvdb_open(kvdb_name.data(), nullptr, &kvdb);
   if (rc) {
+    dout(10) << " failed to open the kvdb" << dendl;
     goto err_out;
   }
 
   /* HSE_TODO: how to handle error logic here */
   rc = hse_kvdb_kvs_open(kvdb, CEPH_METADATA_KVS_NAME.data(), nullptr, &ceph_metadata_kvs);
   if (rc) {
+    dout(10) << " failed to open the ceph-metadata kvs" << dendl;
     goto err_out;
   }
 
   rc = hse_kvdb_kvs_open(kvdb, COLLECTION_OBJECT_KVS_NAME.data(), nullptr, &collection_object_kvs);
   if (rc) {
+    dout(10) << " failed to open the collection-object kvs" << dendl;
     goto err_out;
   }
 
   rc = hse_kvdb_kvs_open(kvdb, OBJECT_DATA_KVS_NAME.data(), nullptr, &object_data_kvs);
   if (rc) {
+    dout(10) << " failed to open the object-data kvs" << dendl;
     goto err_out;
   }
 
   rc = hse_kvdb_kvs_open(kvdb, OBJECT_XATTR_KVS_NAME.data(), nullptr, &object_xattr_kvs);
   if (rc) {
+    dout(10) << " failed to open the object-xattr kvs" << dendl;
     goto err_out;
   }
 
   rc = hse_kvdb_kvs_open(kvdb, OBJECT_OMAP_KVS_NAME.data(), nullptr, &object_omap_kvs);
   if (rc) {
+    dout(10) << " failed to open the object-omap kvs" << dendl;
     goto err_out;
   }
 
@@ -83,36 +90,42 @@ int HseStore::umount()
   /* HSE_TODO: how to handle error logic here */
   rc = hse_kvdb_kvs_close(ceph_metadata_kvs);
   if (rc) {
+    dout(10) << " failed to close the ceph-metadata kvs" << dendl;
     goto err_out;
   }
   ceph_metadata_kvs = nullptr;
 
   rc = hse_kvdb_kvs_close(collection_object_kvs);
   if (rc) {
+    dout(10) << " failed to close the collection-object kvs" << dendl;
     goto err_out;
   }
   collection_object_kvs = nullptr;
 
   rc = hse_kvdb_kvs_close(object_data_kvs);
   if (rc) {
+    dout(10) << " failed to close the object-data kvs" << dendl;
     goto err_out;
   }
   object_data_kvs = nullptr;
 
   rc = hse_kvdb_kvs_close(object_xattr_kvs);
   if (rc) {
+    dout(10) << " failed to close the object-xattr kvs" << dendl;
     goto err_out;
   }
   object_xattr_kvs = nullptr;
 
   rc = hse_kvdb_kvs_close(object_omap_kvs);
   if (rc) {
+    dout(10) << " failed to close the object-omap kvs" << dendl;
     goto err_out;
   }
   object_omap_kvs = nullptr;
 
   rc = hse_kvdb_close(kvdb);
   if (rc) {
+    dout(10) << " failed to close the kvdb" << dendl;
     goto err_out;
   }
   kvdb = nullptr;
@@ -135,36 +148,43 @@ int HseStore::mkfs()
   /* HSE_TODO: how to handle error logic here */
   rc = hse_kvdb_make(kvdb_name.data(), nullptr);
   if (rc) {
+    dout(10) << " failed to make the kvdb" << dendl;
     goto err_out;
   }
 
   rc = hse_kvdb_open(kvdb_name.data(), nullptr, &kvdb);
   if (rc) {
+    dout(10) << " failed to open the kvdb" << dendl;
     goto err_out;
   }
 
   rc = hse_kvdb_kvs_make(kvdb, CEPH_METADATA_KVS_NAME.data(), nullptr);
   if (rc) {
+    dout(10) << " failed to make the ceph-metadata kvs" << dendl;
     goto kvdb_out;
   }
 
   rc = hse_kvdb_kvs_make(kvdb, COLLECTION_OBJECT_KVS_NAME.data(), nullptr);
   if (rc) {
+    dout(10) << " failed to make the collection-object kvs" << dendl;
     goto kvdb_out;
   }
 
   rc = hse_kvdb_kvs_make(kvdb, OBJECT_DATA_KVS_NAME.data(), nullptr);
   if (rc) {
+    dout(10) << " failed to make the object-data kvs" << dendl;
     goto kvdb_out;
   }
 
   rc = hse_kvdb_kvs_make(kvdb, OBJECT_XATTR_KVS_NAME.data(), nullptr);
   if (rc) {
+    dout(10) << " failed to make the object-xattr kvs" << dendl;
     goto kvdb_out;
   }
 
   rc = hse_kvdb_kvs_make(kvdb, OBJECT_OMAP_KVS_NAME.data(), nullptr);
   if (rc) {
+    dout(10) << " failed to make the object-omap kvs" << dendl;
     goto kvdb_out;
   }
 
